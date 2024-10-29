f326b4ef0b0cd1524f24169350cf8a2de94b6299
veerababu <penkeyveerababu229@gmail.com>
2024-10-29 10:58:10 +0530

    update backend url for images

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ app.use('/images', express.static('upload/images'))
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${PORT}/images/${req.file.filename}`
+        image_url: `${process.env.Backend_URL}/images/${req.file.filename}`
     })
 })
 
